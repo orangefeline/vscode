e226d3d4aba3e697a21c8e4c880748ec5db2d93a
Roy:b <rodrigo.anothermen@gmail.com>
2023-11-23 12:04:40 -0600

    Initial commit

---
+++ aña/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const confirmarBtn = document.getElementById('confirmarBtn');
+  
+    confirmarBtn.addEventListener('click', function () {
+      alert('¡Gracias por confirmar tu asistencia! Nos vemos en la celebración.');
+    });
+  });
+  
